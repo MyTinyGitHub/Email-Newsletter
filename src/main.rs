18c7ad30c20b3929bb4874f9eb8fa7e0d0892f8d
MyTinyGitHub <61841849+MyTinyGitHub@users.noreply.github.com>
2023-06-18 08:14:24 +0000

    Dynamic port assignment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run("127.0.0.1:8000")?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,13 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,7 +17,12 @@ async fn health_check_works() {
 }
 
 fn spawn_app() {
-    let server = zero2prod::run("127.0.0.1:8000").expect("Failed to bind address");
+    let listener = TcpListener::bind(127.0.0.1:0).expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(lisener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
+
+    format!("127.0.0.1:{}", port)
 }
