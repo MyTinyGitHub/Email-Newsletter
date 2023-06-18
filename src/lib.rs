933f3ce6ee8a5591a33ae3d73531c5a61cf30938
MyTinyGitHub <61841849+MyTinyGitHub@users.noreply.github.com>
2023-06-18 12:44:47 +0000

    Validating form data

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscription(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(||  { 
               App::new()
                 .route("/health_check", web::get().to(health_check))
+                .route("/subscription", web::post().to(subscription))
          })
         .listen(listener)?
         .run();
