5d0005e8c0b3ba962617d284ace42cccdf8354ee
MyTinyGit <dominik.pall.eu@gmail.com>
2023-07-08 19:49:42 +0200

    add logger on startup

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 use sqlx::PgPool;
+use actix_web::middleware::Logger;
 
 use crate::routes::health_check;
 use crate::routes::subscription;
@@ -11,6 +12,7 @@ pub fn run(listener: TcpListener, db_pool:PgPool ) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscription))
             .app_data(db_pool.clone())
@@ -19,4 +21,4 @@ pub fn run(listener: TcpListener, db_pool:PgPool ) -> Result<Server, std::io::Er
     .run();
 
     Ok(server)
-}
+} 
